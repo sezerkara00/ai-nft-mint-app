b0ad43cc991d6272c19b0c97e6493cf48dd847fe
sezerkara00 <71710799+sezerkara00@users.noreply.github.com>
2024-06-24 06:35:01 +0300

    Update route.ts

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -5,6 +5,18 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+async function fetchWithTimeout(resource, options = {}) {
+    const { timeout = 8000 } = options; // 8 saniyelik bir timeout belirledik
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
+    const response = await fetch(resource, {
+        ...options,
+        signal: controller.signal
+    });
+    clearTimeout(id);
+    return response;
+}
+
 export async function POST(req: NextRequest) {
     try {
         const { imagePrompt } = await req.json();
@@ -31,6 +43,10 @@ export async function POST(req: NextRequest) {
             throw new Error("Failed to generate image");
         }
 
+        console.log("Generated image: " + JSON.stringify(response, null, 2));
+        console.log("Generated image URL: " + response.data[0].url);
+        console.log("b64Json: " + response.data[0].b64_json);
+
         return new Response(JSON.stringify({ data: response.data }), {
             status: 200,
             headers: {
@@ -39,7 +55,7 @@ export async function POST(req: NextRequest) {
         });
 
     } catch (error) {
-        console.error("Errorr generating image:", error);
+        console.error("Error generating image:", error);
         return new Response(JSON.stringify({ error: "Failed to generate image due to internal server error" }), {
             status: 500,
             headers: {
